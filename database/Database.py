a69d94d9248d0b4aad9bb271d7cd69a5eb667330
Aditep <topkoka4000@gmail.com>
2021-01-28 21:29:42 +0700

    Update Database.py

--- database/Database.py
+++ database/Database.py
@@ -5,6 +5,6 @@ def conn():
     if platform == "win32":
         mariadb_connection = mariadb.connect(host="192.168.1.2", user='doae', password='1234', database='doae_dev')
     else:
-        mariadb_connection = mariadb.connect(host="10.3.33.185", user='root', password='d0aep@ssw0rd',
+        mariadb_connection = mariadb.connect(host="192.168.1.2", user='doae', password='1234',
                                              database='doae_dev')
     return mariadb_connection
